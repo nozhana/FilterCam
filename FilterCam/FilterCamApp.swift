cee2c34bfb61acc22f06ceaf9c8eb41bc6fe032e
Nozhan A <nozhana@gmail.com>
2025-08-19 19:27:42 +0330

    Initial Commit

---
+++ FilterCam/FilterCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilterCamApp.swift
+//  FilterCam
+//
+//  Created by Nozhan A. on 8/19/25.
+//
+
+import SwiftUI
+
+@main
+struct FilterCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
