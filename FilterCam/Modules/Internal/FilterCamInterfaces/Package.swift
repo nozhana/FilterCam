b5afd3c7ef8fced947d5deef8231bad5e45f175d
Nozhan A. <nozhana@gmail.com>
2025-08-29 17:22:49 +0330

    Introduce FilterCamInterfaces

---
+++ FilterCam/Modules/Internal/FilterCamInterfaces/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FilterCamInterfaces",
+    platforms: [.iOS(.v17)],
+    products: [
+        .library(
+            name: "FilterCamInterfaces",
+            targets: ["FilterCamInterfaces"]),
+    ],
+    dependencies: [
+        .package(path: "../FilterCamShared"),
+    ],
+    targets: [
+        .target(
+            name: "FilterCamInterfaces", dependencies: ["FilterCamShared"]),
+    ]
+)
